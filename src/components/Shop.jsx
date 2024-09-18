fb76f87c85dd703804c1a770a061d2673ac7de4f
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-09-18 12:45:47 +1000

    initial routing setup

---
+++ src/components/Shop.jsx
@@ -0,0 +1,10 @@
+function Shop() {
+  return (
+    <div>
+      <h1>Shop Page</h1>
+      <div>Should also display number of items in shopping cart in nav bar</div>
+    </div>
+  )
+}
+
+export default Shop;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
+import Home from './components/Home'
+import Shop from './components/Shop.jsx'
+import About from './components/About.jsx'
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />, // Common layout that includes the Navbar
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "shop", element: <Shop /> },
+      { path: "about", element: <About /> },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
