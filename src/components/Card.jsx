db1a94ea8bfbae5d03fac604a3f5e7f6c92e3e70
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-10-09 16:28:17 +1100

    some initial tests for buttons in cart component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import '../styles/Card.css'
 
 //prevent 0 from always showing in user input
@@ -15,22 +14,30 @@ function Card({product, updateAmount, addToCart}) {
       <p>Price: {product.price}</p>
       <img src={product.image}></img>
 
-    <div>
-      {/* <p>Current amount: {product.amount}</p> */}
-      <button onClick={() => updateAmount(product.id, product.amount === '' ? 1 : product.amount + 1)}>+</button>
-      <input 
-        type="number"
-        value={product.amount}
-        placeholder="0"
-        onChange={(e) => handleInputChange(product.id, e.target.value)}
-      />
-      <button onClick={() => updateAmount(product.id, Math.max(product.amount-1, 1))}>-</button>
-      <br />
-      <button onClick={addToCart}>Add to Cart</button>
-    </div>
+      <div>
+        {/* <p>Current amount: {product.amount}</p> */}
+        <button onClick={() => updateAmount(product.id, product.amount === '' ? 1 : product.amount + 1)}>+</button>
+        <input 
+          type="number"
+          value={product.amount}
+          placeholder="0"
+          onChange={(e) => handleInputChange(product.id, e.target.value)}
+        />
+        <button onClick={() => updateAmount(product.id, Math.max(product.amount-1, 1))}>-</button>
+        <br />
+        <button onClick={addToCart}>Add to Cart</button>
+      </div>
 
     </div>
   )
+
+  // return (
+  //   <div className="card">
+  //     <p>{product.title}</p>
+  //     <p>Price: {product.price}</p>
+  //     <img src={product.image}></img>
+  //   </div>
+  // )
 }
 
 export default Card;
--- src/tests/Card.test.jsx
+++ src/tests/Card.test.jsx
@@ -1,39 +1,62 @@
 import { vi, describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
-// import { MemoryRouter } from 'react-router-dom';
-// import { CartProvider } from '../context/CartContext';
-import { Card } from '../components/Card';
+import Card from '../components/Card';
 
-test('renders Card component', () => {
+describe("Add to Cart button", () => {
+  it("should render a button with the text 'Add to Cart'", () => {
+    render(<Card product={{}} updateAmount={() => {}} addToCart={() => {}}/>);
 
-  describe("Add to Cart button", () => {
-    it("should render a button with the text 'Add to Cart'", () => {
-      render(<Card product={{}} updateAmount={() => {}} addToCart={() => {}}/>);
+    const button = screen.getByRole("button", { name: "Add to Cart" });
 
-      const button = screen.getByRole("button", { name: "Add to Cart" });
+    expect(button).toBeInTheDocument();
+  });
+  it("should call the add function when clicked", async () => {
+    const add = vi.fn();
+    const user = userEvent.setup()
+    render(<Card product={{}} updateAmount={() => {}} addToCart={add}/>);
 
-      expect(button).toBeInTheDocument();
-    });
-    it("should call the add function when clicked", async () => {
-      const add = vi.fn();
-      const user = userEvent.setup()
-      render(<Card product={{}} updateAmount={() => {}} addToCart={add}/>);
+    const button = screen.getByRole("button", { name: "Add to Cart" });
 
-      const button = screen.getByRole("button", { name: "Add to Cart" });
+    await user.click(button);
 
-      await user.click(button);
+    expect(add).toHaveBeenCalled();
+  });
 
-      expect(add).toHaveBeenCalled();
-    });
+  it("should not call the add function when it isn't clicked", async () => {
+    const add = vi.fn();
+    render(<Card product={{}} updateAmount={() => {}} addToCart={add}/>);
 
-    it("should not call the add function when it isn't clicked", async () => {
-      const add = vi.fn();
-      render(<Card product={{}} updateAmount={() => {}} addToCart={add}/>);
-
-      expect(onClick).not.toHaveBeenCalled();
-    });
+    expect(add).not.toHaveBeenCalled();
   });
 });
+
+describe("renders products info", () => {
+  it("should render product's name and price", () => {
+    const product = {
+      id: 11,
+      title: 'yoyo',
+      price: 5.0,
+      amount: 1
+    }
+    render(<Card product={product} updateAmount={() => {}} addToCart={() => {}}/>);
+    const title = screen.getByText("yoyo");
+    const price = screen.getByText("Price: 5");
+    expect(title).toBeInTheDocument();
+    expect(price).toBeInTheDocument();
+  })
+});
+
+describe("buttons interaction", () => {
+  it("should first renders increment and decrement buttons", () => {
+    render(<Card product={{}} updateAmount={() => {}} addToCart={() => {}}/>);
+    const increment = screen.getByRole("button", { name: "+" });
+    const decrement = screen.getByRole("button", { name: "-" });
+    screen.debug();
+
+    expect(increment).toBeInTheDocument();
+    expect(decrement).toBeInTheDocument();
+  })
+})
 
 //TODO: mock some data
