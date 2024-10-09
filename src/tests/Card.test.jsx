edf5d2ce4a96d756a6d49ea6c1287251ab3b27e0
Minh Tran <115627819+minhtran5109@users.noreply.github.com>
2024-10-09 15:16:36 +1100

    userEvent in testing-library now work properly

--- src/tests/Card.test.jsx
+++ src/tests/Card.test.jsx
@@ -1,6 +1,6 @@
 import { vi, describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react';
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 // import { MemoryRouter } from 'react-router-dom';
 // import { CartProvider } from '../context/CartContext';
 import { Card } from '../components/Card';
@@ -15,7 +15,24 @@ test('renders Card component', () => {
 
       expect(button).toBeInTheDocument();
     });
-    
+    it("should call the add function when clicked", async () => {
+      const add = vi.fn();
+      const user = userEvent.setup()
+      render(<Card product={{}} updateAmount={() => {}} addToCart={add}/>);
+
+      const button = screen.getByRole("button", { name: "Add to Cart" });
+
+      await user.click(button);
+
+      expect(add).toHaveBeenCalled();
+    });
+
+    it("should not call the add function when it isn't clicked", async () => {
+      const add = vi.fn();
+      render(<Card product={{}} updateAmount={() => {}} addToCart={add}/>);
+
+      expect(onClick).not.toHaveBeenCalled();
+    });
   });
 });
 
